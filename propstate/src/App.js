3dc4022f60c17b6fbfbae4851017cdde21c6947f
leehosu <hosu4549@gmail.com>
2019-07-05 19:52:46 +0900

    Update README and Add New Study that Personal Study about Concept Of Props and State 🧛‍

---
+++ propstate/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MyName from './MyName';
+
+class App extends Component {
+  render() {
+    return (
+      <MyName />
+    );
+  }
+}
+
+export default App;
