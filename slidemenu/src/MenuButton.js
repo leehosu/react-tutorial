63cf86698fc663fc76e6f090b69a16bc51b6f8b9
leehosu <hosu4549@gmail.com>
2019-07-09 20:27:06 +0900

    Update README and Study The Chapter 17 for Protect Unneccesary Rendering 🔧

--- slidemenu/src/MenuButton.js
+++ slidemenu/src/MenuButton.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 import "./MenuButton.css";
 
 class MenuButton extends Component{
+    shouldComponentUpdate(nextProps, nextState){
+        if(nextProps.handleMouseDown === this.props.handleMouseDown){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
     render() {
+        console.log("Rendering : MenuButton");
         return(
             <button id = "roundButton"
                     onMouseDown = {this.props.handleMouseDown} />
