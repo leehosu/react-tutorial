7036b00cd40421ede79cb6d92692f7bdba7dd1f6
leehosu <hosu4549@gmail.com>
2019-08-30 18:28:33 +0900

    Study the REACT of Array that PhoneBook about Delete and update

--- input_controll/src/components/PhoneInfo.js
+++ input_controll/src/components/PhoneInfo.js
@@ -8,6 +8,49 @@ class PhoneInfo extends Component{
             id : 0
         }
     }
+
+    state = {
+        editing : false,
+        name : '',
+        phone : ''
+    }
+
+    handleRemove = () => {
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+
+    handleToggleEdit = () => {
+        const { editing } = this.state;
+        this.setState({
+            editing : !editing
+        })
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name] : value
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        const { info, onUpdate } = this.props;
+        if(!prevState.editing && this.state.editing){
+            this.setState({
+                name : info.name,
+                phone : info.phone
+            })
+        }
+        
+    if(prevState.editing && !this.state.editing){
+        onUpdate(info.id, {
+            name : this.state.name,
+            phone : this.state.phone
+        });
+    }
+    }
+
     render(){
 
         const style = {
@@ -15,6 +58,38 @@ class PhoneInfo extends Component{
             padding: '8px',
             margin: '8px'
         };
+
+        const { editing } = this.state;
+
+        if(editing){
+            if (editing) { // 수정모드
+                return (
+                  <div style={style}>
+                    <div>
+                      <input
+                        value={this.state.name}
+                        name="name"
+                        placeholder="이름"
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        value={this.state.phone}
+                        name="phone"
+                        placeholder="전화번호"
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <button onClick={this.handleToggleEdit}>적용</button>
+                    <button onClick={this.handleRemove}>삭제</button>
+                  </div>
+                );
+              }
+        }
+
+
+
 
         const {
             name, phone
@@ -24,6 +99,8 @@ class PhoneInfo extends Component{
             <div className = "App-main" style = {style}>
                 <div><b>{name}</b></div>
                 <div>{phone}</div>
+                <button onClick={this.handleToggleEdit}>수정</button>
+                <button onClick={this.handleRemove}>삭제</button>
             </div>
 
         );
